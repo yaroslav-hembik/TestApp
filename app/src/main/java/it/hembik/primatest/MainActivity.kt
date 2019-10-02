a6918bf03547fd01d108f047d4bc42acd3c66292
Yaroslav Hembik <yaroslav.hembik@amaris.com>
2019-10-02 18:37:29 +0200

    First commit for test app

---
+++ app/src/main/java/it/hembik/primatest/MainActivity.kt
@@ -0,0 +1,12 @@
+package it.hembik.primatest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
